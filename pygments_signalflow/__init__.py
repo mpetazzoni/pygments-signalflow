eb5f5ddead72efc70e1b5923de965cfdb6e83f31
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2016-06-17 16:12:26 -0700

    Updates and simplifications
    Don't need the complexity of Python string formatting in the string
    lexing. Fix function/builtin argument parsing and token identification
    by utilizing the stack/context feature.

--- pygments_signalflow/__init__.py
+++ pygments_signalflow/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2016 Maxime Petazzoni <maxime.petazzoni@bulix.org>
 
-from pygments.lexer import RegexLexer, bygroups, combined, include, words
+from pygments.lexer import RegexLexer, bygroups, include
 from pygments.token import *  # noqa
 
 
@@ -15,34 +15,27 @@ class SignalFlowLexer(RegexLexer):
     aliases = ['signalflow', 'flow']
     filenames = ['*.flow']
 
-    def innerstring_rules(ttype):
-        return [
-            # the old style '%s' % (...) string formatting
-            (r'%(\(\w+\))?[-#0 +]*([0-9]+|[*])?(\.([0-9]+|[*]))?'
-             '[hlL]?[E-GXc-giorsux%]', String.Interpol),
-            (r'[^\\\'"%\n]+', ttype),
-            (r'[\'"\\]', ttype),
-            (r'%', ttype),
-        ]
-
     tokens = {
         'root': [
             (r'\s+', Text),
             (r'#.*$', Comment.Single),
             (r'[(),.:]', Punctuation),
             (r'!=|[-*+%/<>=]', Operator),
-            (r'(?<=[(,])\w+(?=[\s=])', Keyword),
-            include('builtins'),
+            (r'(abs|accumulator|bottom|ciel|const|count|data|delta|detect'
+             '|dimensionalize|events|extrapolate|fetch|filter|find|floor'
+             '|graphite|groupby|id|integrate|log|log10|map|math|max|mean'
+             '|mean_plus_stddev|median|min|newrelic|percentile|pow|print'
+             '|publish|random|rateofchange|sample|sample_stddev'
+             '|sample_variance|select|size|split|sqrt|stats|stddev|sum'
+             '|threshold|timeshift|top|variance|when|window)(\()',
+             bygroups(Name.Builtin, Punctuation), 'function'),
+            (r'(_collector|_random|_seq|_turnstile)(\()',
+             bygroups(Name.Function.Magic, Punctuation), 'function'),
+            (r'lambda(?=\s)', Keyword.Reserved),
             include('name'),
             include('numbers'),
-            ('([rR]|[uUbB][rR]|[rR][uUbB])(")',
-             bygroups(String.Affix, String.Double), 'dqs'),
-            ("([rR]|[uUbB][rR]|[rR][uUbB])(')",
-             bygroups(String.Affix, String.Single), 'sqs'),
-            ('([uUbB]?)(")', bygroups(String.Affix, String.Double),
-             combined('stringescape', 'dqs')),
-            ("([uUbB]?)(')", bygroups(String.Affix, String.Single),
-             combined('stringescape', 'sqs')),
+            ('"', String.Double, 'dqs'),
+            ("'", String.Single, 'sqs'),
         ],
 
         'name': [
@@ -57,39 +50,20 @@ class SignalFlowLexer(RegexLexer):
             (r'\d+', Number.Integer),
         ],
 
-        'stringescape': [
-            (r'\\([\\abfnrtv"\']|\n|N\{.*?\}|u[a-fA-F0-9]{4}|'
-             r'U[a-fA-F0-9]{8}|x[a-fA-F0-9]{2}|[0-7]{1,3})', String.Escape)
-        ],
-        'strings-single': innerstring_rules(String.Single),
-        'strings-double': innerstring_rules(String.Double),
         'dqs': [
             (r'"', String.Double, '#pop'),
             (r'\\\\|\\"|\\\n', String.Escape),  # included here for raw strings
-            include('strings-double')
+            (r'[^"]', String.Double),
         ],
         'sqs': [
             (r"'", String.Single, '#pop'),
             (r"\\\\|\\'|\\\n", String.Escape),  # included here for raw strings
-            include('strings-single')
+            (r"[^']", String.Single),
         ],
 
-        'builtins': [
-            (words(('abs', 'accumulator', 'bottom', 'ciel', 'const', 'count',
-                    'data', 'delta', 'detect', 'dimensionalize', 'events',
-                    'extrapolate', 'fetch', 'filter', 'find', 'floor',
-                    'graphite', 'groupby', 'id', 'integrate', 'log', 'log10',
-                    'map', 'math', 'max', 'mean', 'mean_plus_stddev', 'median',
-                    'min', 'newrelic', 'percentile', 'pow', 'print', 'publish',
-                    'random', 'rateofchange', 'sample', 'sample_stddev',
-                    'sample_variance', 'select', 'size', 'split', 'sqrt',
-                    'stats', 'stddev', 'sum', 'threshold', 'timeshift', 'top',
-                    'variance', 'when', 'window'),
-                   suffix=r'(?=\()'),
-                Name.Builtin),
-            (words(('_collector', '_random', '_seq', '_turnstile'),
-                   suffix=r'\b'),
-                Name.Function.Magic),
-            (words(('lambda'), suffix=r'(?=\s)'), Keyword.Reserved),
-            ],
+        'function': [
+            (r'\)', Punctuation, '#pop'),
+            (r'\w+(?=[\s=])', Keyword),
+            include('root'),
+        ],
     }
